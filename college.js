6231b69bd5d00cd5aaa7a3ac324cab74505ce58a
yamuna1996d <yamunag19@gmail.com>
2020-04-02 18:50:53 +0530

    expess-post-git-heroku

--- college.js
+++ college.js
@@ -3,16 +3,16 @@ var express =require('express');
 var parser=require('body-parser');
 var app=express();
 app.use(parser.urlencoded({extended:false}));
-// app.get('/',(req,res)=>{
-//     res.send("hai..");
-// })
 app.get('/',(req,res)=>{
+    res.send("hai..");
+});
+app.get('/register',(req,res)=>{
     var na = req.body.getname;
     var roll=req.body.getroll;
     var adno=req.body.getadno;
     var college=req.body.getcollege;
     res.send("Name" + na + "Rollno :" +roll + "Admission Number" + adno +"College" + college);
-    // res.json(req.body);
+    res.json(req.body);
 });
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server Started");
