402095a57e5e50edb0b1cfdf2bfdac1d53108b80
yamuna1996d <yamunag19@gmail.com>
2020-04-04 09:55:49 +0530

    expess-post-git-heroku

--- college.js
+++ college.js
@@ -1,18 +1,41 @@
 var express = require('express');
 var express =require('express');
 var parser=require('body-parser');
+var mongoose = require('mongoose');
+
 var app=express();
 app.use(parser.urlencoded({extended:false}));
-// app.get('/',(req,res)=>{
-//     res.send("hai..");
-// });
+
+const studentSchema = new mongoose.Schema(
+    {
+        name: String,
+        roll:Number,
+        adno:Number,
+        college: String
+    }//creating Schema
+); 
+const studmodel = mongoose.model('students',studentSchema);//creating model
+mongoose.connect("mongodb+srv://dbuser:ava1996@cluster0-ffwzj.mongodb.net/test?retryWrites=true&w=majority");
 app.get('/',(req,res)=>{
-    var na = req.body.getname;
-    var roll=req.body.getroll;
-    var adno=req.body.getadno;
-    var college=req.body.getcollege;
-    res.send("Name" + na + "Rollno :" +roll + "Admission Number" + adno +"College" + college);
-    res.json(req.body);
+    res.send("hai..");
+});
+app.get('/register',(req,res)=>{
+    // var getna = req.body.name;
+    // var getroll=req.body.roll;
+    // var getadno=req.body.adno;
+    // var getcollege=req.body.college;
+    //res.send("Name" + na + "Rollno :" +roll + "Admission Number" + adno +"College" + college);
+
+    try{
+        var studentdata= new studmodel(req.body);
+        var result=await studentdata.save();
+        res.json(result);
+        //res.json(req.body);
+    }
+    catch (error){
+        console.log(error);
+        res.status(500).send(error);
+    }
 });
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server Started");
